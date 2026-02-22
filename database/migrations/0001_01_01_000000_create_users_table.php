2f8c289cb6909ccde5cb4457578db5b0c2b26d06
Kaizen <gibrantsyareza@gmail.com>
2026-02-22 16:01:36 +0700

    updating the user migration schema so it fulfill my requirement for this project

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
+            $table->integer('nis')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_voted')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
