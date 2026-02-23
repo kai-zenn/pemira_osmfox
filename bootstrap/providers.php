d32d71e3dafc28021b4387725c483e501eab8c1b
Kaizen <gibrantsyareza@gmail.com>
2026-02-23 16:50:49 +0700

    menambahkan filament untuk membangun admin panel yang cepat

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    Livewire\LivewireServiceProvider::class,
 ];
