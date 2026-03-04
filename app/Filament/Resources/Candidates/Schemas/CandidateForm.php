76c96fc6812b49a0b5031f81d923c25358c45cb3
Kaizen <gibrantsyareza@gmail.com>
2026-03-04 11:33:02 +0700

    menambahkan halaman dashboard di filament untuk melakukan operasi CRUD terhadap kandidat, nantinya data akan di tampilkan dengan komponen livewire

---
+++ app/Filament/Resources/Candidates/Schemas/CandidateForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Candidates\Schemas;
+
+use Filament\Schemas\Schema;
+
+class CandidateForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
