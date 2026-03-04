7b83d2925ce20c1028ebcc6e79080fb7cbf9d64e
Kaizen <gibrantsyareza@gmail.com>
2026-03-04 10:35:01 +0700

    menambahkan routing untuk halaman main dengan middleware auth untuk mengecek kredensial user

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use resources\pages\auth\login;
 
-// Route::livewire('/login', 'pages::auth.login')->name('login');
+Route::livewire('/', 'pages::guest.landing')->name('landingPage');
+Route::middleware(['auth'])->group(function (){
+    Route::livewire('/home', 'pages::main.home')->name('home');
+});
 
 // Route::get('/', function () {
 //     return view('welcome');
